17155f25a07a982092efb21a0a4564c6d2b78b27
Kusa Lechisa <kusalechisac@gmail.com>
2026-05-15 16:27:05 +0900

    Add service worker unregistration logic to main entry point

--- Job-Application-FrontEnd/src/main.jsx
+++ Job-Application-FrontEnd/src/main.jsx
@@ -4,6 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from "./context/ThemeContext";
 
+// Unregister any existing service workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(registration => {
+      registration.unregister().then(() => {
+        console.log('Service worker unregistered');
+      });
+    });
+  });
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
