ad7ffd3e11af74a11c42cb5f6f4d36d1a34ab8ba
Kusa Lechisa <kusalechisac@gmail.com>
2026-05-15 17:58:06 +0900

    Update Prisma setup to include adapter for PostgreSQL and adjust client import; enhance database migration script

--- Job-Application-BackEnd/src/prisma.js
+++ Job-Application-BackEnd/src/prisma.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.ts";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 export { prisma };
