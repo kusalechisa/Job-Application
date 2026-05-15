cf0d45ed0cdf6b37389372a8a75afa482af814c0
Kusa Lechisa <kusalechisac@gmail.com>
2026-05-15 14:58:52 +0900

    Enhance ProtectedRoute component to include token validation for access control

--- Job-Application-FrontEnd/src/components/ProtectedRoute.jsx
+++ Job-Application-FrontEnd/src/components/ProtectedRoute.jsx
@@ -2,9 +2,9 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export default function ProtectedRoute({ children, role }) {
-  const { user } = useAuth();
-  if (!user) return <Navigate to="/" />;
-  if (role && user.role !== role) return <Navigate to="/" />;
+  const { user, token } = useAuth();
+  if (!user && !token) return <Navigate to="/" />;
+  if (role && user && user.role !== role) return <Navigate to="/" />;
 
   return children;
 }
