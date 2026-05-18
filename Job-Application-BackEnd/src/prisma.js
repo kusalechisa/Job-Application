95bb0f389d3a426281e3760bff2a697d8b55e6cf
Kusa Lechisa <kusalechisac@gmail.com>
2026-05-18 23:14:32 +0900

    render

--- Job-Application-BackEnd/src/prisma.js
+++ Job-Application-BackEnd/src/prisma.js
@@ -1,8 +1,3 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
+export const prisma = new PrismaClient();
